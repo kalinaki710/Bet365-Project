66d061babb5917305daf48934a448c4b25c039bc
Kalin Angelov <kalin.zlatkov.angelov@gmail.com>
2021-10-06 22:05:28 +0100

    10

--- script.js
+++ script.js
@@ -40,7 +40,7 @@
           const button = document.createElement("button");
           button.type = "button";
           button.innerHTML = "&#9660; " + addInput.value;
-          button.onclick = function () {  
+          button.onclick = function () {
             let child = this.nextElementSibling;
             console.log(child)
             if (child.style.display === "block") {
@@ -74,14 +74,49 @@
              newItem.classList.add("listItem");
              const checkmarkCircle = document.createElement("span");
              checkmarkCircle.className = "circle";
+             checkmarkCircle.addEventListener("click", function(){
+               if (this.style. background === "green") {
+                 this.style.background = "#eee";
+               } else {
+                 this.style.background = "green";
+                 }
+             });
              const checkmarkTick = document.createElement("span");
              checkmarkTick.className = "tick";
-             newItem.innerHTML = input.value;
+             let label = document.createElement("label");
+             label.innerHTML = input.value;
+             label.setAttribute("readonly", "true");
+             newItem.appendChild(label);
              newItem.appendChild(checkmarkCircle);
              newItem.appendChild(checkmarkTick);
              const quantity = document.createElement("input");
              quantity.type = "number";
              newItem.appendChild(quantity);
+             const deleteButton = document.createElement("button");
+             deleteButton.innerHTML = "Delete";
+             deleteButton.onclick = function () {
+               newItem.remove();
+             };
+             newItem.appendChild(deleteButton);
+             const editButton = document.createElement("button");
+             editButton.innerHTML = "Edit";
+             editButton.onclick = function () {
+               if (label.style.display != "none"){
+                 label.style.display = "none";
+                 textBox = document.createElement("input");
+                 textBox.type = "input";
+                 textBox.className = "newItemInput";
+                 textBox.value = label.innerHTML;
+                 label.parentNode.insertBefore(textBox, label);
+               }else {
+                 label.style.display = "inline";
+                 textBox = label.previousElementSibling;
+                 label.innerHTML = textBox.value;
+                 textBox.remove();
+               }
+
+             };
+             newItem.appendChild(editButton);
              newList.appendChild(newItem);
             }
           }
